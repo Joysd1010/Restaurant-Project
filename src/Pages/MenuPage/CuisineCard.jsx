0025f5b852957e39b36ecbd6160e3b4230f162d5
Joy Sutradhar <joysutradharaj@gmail.com>
2024-08-14 05:06:13 +0600

    Menu Page is completed

--- src/Pages/MenuPage/CuisineCard.jsx
+++ src/Pages/MenuPage/CuisineCard.jsx
@@ -1,47 +1,36 @@
 import PropTypes from "prop-types";
 
-const CusineCard = ({ menu }) => {
-  const { image, name, details, offer, price, offerPrice } = menu;
-
+const CusineCard = ({ menu, onWarningClick }) => {
+  const { image, name, promotionalLine, offer, price, offerPrice,available } = menu;
+ 
   return (
-    <>
-      <div className="bg-white shadow-lg w-72 mx-auto  rounded-lg overflow-hidden hover:shadow-limeGreen duration-300">
-        <img src={image} alt={name} className="w-full p-2" />
-        <div className="p-4">
-          <h3 className="text-xl font-bold mb-2">{name}</h3>
-          <p className="text-gray-700">{details}</p>
-         <div className=" flex justify-between items-center
-         ">
-            <div>
-              <p className="text-gray-900 font-semibold mt-2">
-                ${offer ? offerPrice : price}
-              </p>
-              {offer && <p className="text-red-500 line-through">${price}</p>}
-            </div>
-            <button
-              className="btn btn-outline btn-warning "
-              onClick={() => console.log(name)}
-            >
-              Warning
-            </button>
-         </div>
+    <div className="bg-white shadow-lg w-72 mx-auto rounded-lg overflow-hidden hover:shadow-limeGreen duration-300">
+      <img src={image} alt={name} className="w-full p-2" />
+     {offer&& <div className="bg-red-700 border2 rotate-45 bottom-52 left-20 relative text-center text-white  font-merriweather text-18">
+        offer
+      </div>}
+      <div className="p-4">
+        <h3 className="text-xl font-bold mb-2">{name}</h3>
+        <p className="text-gray-700">{promotionalLine}</p>
+        <div className=" flex justify-between items-center py-3">
+          <div>
+            <p className="text-gray-900 font-semibold mt-2">
+              ${offer ? offerPrice : price}
+            </p>
+            {offer && <p className="text-red-500 line-through">${price}</p>}
+          </div>
+          <button
+            className="btn btn-outline btn-warning "
+            onClick={onWarningClick}
+          >
+            See Details
+          </button>
         </div>
       </div>
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-              ✕
-            </button>
-          </form>
-          <h3 className="font-bold text-lg">{name}</h3>
-          <p className="py-4">Press ESC key or click on ✕ button to close</p>
-        </div>
-      </dialog>
-    </>
+    </div>
   );
 };
+
 CusineCard.propTypes = {
   menu: PropTypes.shape({
     image: PropTypes.string.isRequired,
@@ -51,5 +40,7 @@ CusineCard.propTypes = {
     price: PropTypes.number.isRequired,
     offerPrice: PropTypes.number,
   }).isRequired,
+  onWarningClick: PropTypes.func.isRequired,
 };
+
 export default CusineCard;
--- src/Pages/MenuPage/MenuByCategory.jsx
+++ src/Pages/MenuPage/MenuByCategory.jsx
@@ -4,6 +4,7 @@ import CusineCard from "./CuisineCard";
 import { useParams } from "react-router-dom";
 import { MdOutlineOutdoorGrill } from "react-icons/md";
 import SetMenu from "./SetMenu";
+import { TfiGift } from "react-icons/tfi";
 
 const MenuByCategory = () => {
   const { category } = useParams(); // Extract category from URL parameters
@@ -13,17 +14,17 @@ const MenuByCategory = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 9;
 
-  // Fetch menu items by category when activeCategory changes
   useEffect(() => {
     const params =
       activeCategory === "All" || activeCategory === "Set Menu"
         ? {}
         : { category: activeCategory };
-
+  
     axiosInstance
       .get(`/menu`, { params })
       .then((response) => {
-        setMenu(response.data);
+        const availableItems = response.data.filter(item => item.available === true);
+        setMenu(availableItems);
         setCurrentPage(1); // Reset to the first page whenever category changes
       })
       .catch((error) => {
@@ -31,7 +32,6 @@ const MenuByCategory = () => {
       });
   }, [activeCategory]);
 
-  // Fetch unique categories
   useEffect(() => {
     axiosInstance
       .get("/menu/category")
@@ -53,7 +53,7 @@ const MenuByCategory = () => {
   };
 
   const handleNextPage = () => {
-    const maxPage = Math.ceil(length / itemsPerPage);
+    const maxPage = Math.ceil(menu.length / itemsPerPage);
     setCurrentPage((prevPage) => Math.min(prevPage + 1, maxPage));
   };
 
@@ -61,16 +61,40 @@ const MenuByCategory = () => {
     setCurrentPage(pageNumber);
   };
 
-  // Calculate the items to display on the current page
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentMenuItems = menu.slice(indexOfFirstItem, indexOfLastItem);
- 
-  // Calculate total pages
+
   const totalPages = Math.ceil(menu.length / itemsPerPage);
 
+  const [selectedMenuItem, setSelectedMenuItem] = useState({});
+
+  const handleWarningClick = (menuItem) => {
+    setSelectedMenuItem(menuItem);
+    document.getElementById("my_modal_3").showModal();
+  };
+
+  const {
+    name,
+    image,
+    details,
+    ingredients,
+    price,
+    offer,
+    offerPrice,
+  } = selectedMenuItem;
+
+  const lunchMenu=menu.filter(
+    (item) => item.type && item.type.toLowerCase() === 'lunch'
+  )
+  const dinnerMenu=menu.filter(
+    (item) => item.type && item.type.toLowerCase() === 'dinner'
+  )
+  const breakFastMenu=menu.filter(
+    (item) => item.type && item.type.toLowerCase() === 'breakfast'
+  )
   return (
-    <div className="md:mx-28 ">
+    <div className="md:mx-28">
       <h1 className="text-center font-bold text-28 py-5 font-merriweather">
         Find your desired dish
       </h1>
@@ -114,42 +138,94 @@ const MenuByCategory = () => {
       </div>
 
       {activeCategory === "Set Menu" ? (
-        <div className=" px-3">
+        <div className="px-3">
           <div className="flex flex-col items-center py-3">
-            <MdOutlineOutdoorGrill size={30} className=" text-green-400"/>
+            <MdOutlineOutdoorGrill size={30} className="text-green-400" />
             <h1 className="text-20 font-merriweather font-semibold text-Charcoal">
               Breakfast
             </h1>
           </div>
-          <hr className="border-2 mb-4 mx-20"/>
-                   <SetMenu menu={menu} time={'breakfast'}/>
+          <hr className="border-2 mb-4 mx-20" />
+          {
+            breakFastMenu.length < 1 ? (
+              <div className="font-merriweather text-18 text-center font-bold py-5">
+                Our Dinner menu is on the way, please stay tuned
+              </div>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 py-3">
+                {breakFastMenu.map((foods) => (
+                  <CusineCard
+                  key={foods._id}
+                  menu={foods} // Pass the menu item data
+                  onWarningClick={() => handleWarningClick(foods)}
+                  />
+                
+                ))}
+              </div>)
+          }
           <div className="flex flex-col items-center py-3">
-            <MdOutlineOutdoorGrill size={30} className=" text-green-400"/>
+            <MdOutlineOutdoorGrill size={30} className="text-green-400" />
             <h1 className="text-20 font-merriweather font-semibold text-Charcoal">
               Lunch
             </h1>
           </div>
-          <hr className="border-2 mb-4 mx-20"/>
-                   <SetMenu menu={menu} time={'lunch'}/>
+          <hr className="border-2 mb-4 mx-20" />
+          {
+            lunchMenu.length < 1 ? (
+              <div className="font-merriweather text-18 text-center font-bold py-5">
+                Our Dinner menu is on the way, please stay tuned
+              </div>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 py-3">
+                {lunchMenu.map((foods) => (
+                  <CusineCard
+                  key={foods._id}
+                  menu={foods} // Pass the menu item data
+                  onWarningClick={() => handleWarningClick(foods)}
+                  />
+                
+                ))}
+              </div>)
+          }
           <div className="flex flex-col items-center py-3">
-            <MdOutlineOutdoorGrill size={30} className=" text-green-400"/>
+            <MdOutlineOutdoorGrill size={30} className="text-green-400" />
             <h1 className="text-20 font-merriweather font-semibold text-Charcoal">
               Dinner
             </h1>
           </div>
-          <hr className="border-2 mb-4 mx-20"/>
-                   <SetMenu menu={menu} time={'dinner'}/>
-
+          <hr className="border-2 mb-4 mx-20" />
+          {/* <SetMenu menu={menu} time="dinner" onWarningClick={handleWarningClick} /> */}
+          {
+            dinnerMenu.length < 1 ? (
+              <div className="font-merriweather text-18 text-center font-bold py-5">
+                Our Dinner menu is on the way, please stay tuned
+              </div>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 py-3">
+                {dinnerMenu.map((foods) => (
+                  <CusineCard
+                  key={foods._id}
+                  menu={foods} // Pass the menu item data
+                  onWarningClick={() => handleWarningClick(foods)}
+                  />
+                
+                ))}
+              </div>)
+          }
         </div>
       ) : (
-        <div className=" py-5 px-3">
+        <div className="py-5 px-3">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-            {currentMenuItems.map((menuItem, index) => (
-              <CusineCard key={menuItem._id} menu={menuItem} />
+            {currentMenuItems.map((menuItem) => (
+              <CusineCard
+                key={menuItem._id}
+                menu={menuItem}
+                onWarningClick={() => handleWarningClick(menuItem)}
+              />
             ))}
           </div>
 
-          <div className=" flex justify-around">
+          <div className="flex justify-around">
             <div className="flex gap-5 items-center mt-5">
               <button
                 className="px-4 py-2 text-white bg-lime rounded hover:bg-yellow-300 hover:text-Charcoal"
@@ -188,6 +264,52 @@ const MenuByCategory = () => {
             </div>
           </div>
         </div>
+      )}
+
+      {selectedMenuItem && (
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box bg-warm">
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+            <div>
+              <img src={image} alt={name} className="w-60 mx-auto" />
+              <h1 className="font-bold text-22 text-Charcoal py-2">{name}</h1>
+
+              {offer ? (
+                <div className="flex justify-between items-center">
+                  <div className="flex gap-2">
+                    <p className="text-Charcoal line-through font-bold text-[16px]">
+                      {price} $
+                    </p>
+                    <p className="text-lime font-bold text-[16px]">
+                      {offerPrice} $
+                    </p>
+                  </div>
+
+                  <div className="flex gap-2 items-center font-bold text-18 px-2 py-1 rounded bg-olive text-warm">
+                    <TfiGift size={20} /> Offer
+                  </div>
+                </div>
+              ) : (
+                <p className="text-Charcoal font-bold text-[16px]">{price} $</p>
+              )}
+              <p className="text-18 font-merriweather">{details}</p>
+              {ingredients && (
+                <div>
+                  <p className="text-lime font-bold text-[17px]">
+                    Ingredients :{" "}
+                    <span className="text-Charcoal text-[17px] font-normal">
+                      {ingredients.join(", ")}
+                    </span>
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+        </dialog>
       )}
     </div>
   );
