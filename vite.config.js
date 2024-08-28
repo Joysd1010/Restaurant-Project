8ebca1a2de2e084657a544ed3be793441905dea3
Joy Sutradhar <joysutradharaj@gmail.com>
2024-08-28 18:48:09 +0600

    changing some vercel error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Set the limit to 500KB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Create a separate chunk for vendor libraries
+          }
+          if (id.includes('specific-directory')) {
+            return 'specific-chunk'; // Create a chunk for specific modules
+          }
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+});
