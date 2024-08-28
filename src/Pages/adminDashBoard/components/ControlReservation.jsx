7c71d66fd2ddfd3e934cc434abd0f34c99b6b927
Joy Sutradhar <joysutradharaj@gmail.com>
2024-08-29 02:36:30 +0600

    initial

--- src/Pages/adminDashBoard/components/ControlReservation.jsx
+++ src/Pages/adminDashBoard/components/ControlReservation.jsx
@@ -65,8 +65,11 @@ const ControlReservation = () => {
             className="mt-1 block w-full px-4 py-2 bg-white text-black border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
         </div>
+
         {
-            
+            timeSlot.map((item)=><div key={item._id}>
+                <h1>{item.time}</h1>
+            </div>)
         }
 
       </div>
